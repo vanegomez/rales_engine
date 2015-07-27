e0c11b84d4b887d5b878510bb10a7289fba398e5
vanegomez <vane.gomez@me.com>
2015-07-27 14:02:06 -0600

    customer model and test created

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:customer) { Customer.create(first_name: "Cookie", last_name: "Monster") }
+
+  it 'is valid' do
+    expect(customer).to be_valid
+  end
+
+  it 'is invalid without a first name' do
+    customer.first_name = nil
+    expect(customer).to_not be_valid
+  end
+
+  it 'is invalid without a last name' do
+    customer.last_name = nil
+    expect(customer).to_not be_valid
+  end
 end
