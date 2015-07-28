574cac3819d89a2b68554e3220b2f3741109bd6c
vanegomez <vane.gomez@me.com>
2015-07-28 09:56:59 -0600

    invoices crontoller created

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::InvoicesController < ApplicationController
+  respond_to :json, :xml
+
+  def index
+    respond_with Invoice.all
+  end
+
+  def show
+  respond_with Invoice.find_by(id: params[:id])
+  end
 end
