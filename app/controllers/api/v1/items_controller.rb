13d06b5457d1e4dfad9b9c7bec7cfb55d3ec453e
vanegomez <vane.gomez@me.com>
2015-07-28 09:58:33 -0600

    merchant crontoller created

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::ItemsController < ApplicationController
   respond_to :json, :xml
 
   def index
-    respond_with Items.all
+    respond_with Item.all
   end
 
   def show
-    respond_with Items.find_by(id: params[:id])
+    respond_with Item.find_by(id: params[:id])
   end
 end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::MerchantsController < ApplicationController
+  respond_to :json, :xml
+
+  def index
+    respond_with Merchant.all
+  end
+
+  def show
+    respond_with Merchant.find_by(id: params[:id])
+  end
 end
