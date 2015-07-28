9adc87201686b2c1cbab6b3df041184da20836d2
vanegomez <vane.gomez@me.com>
2015-07-28 09:57:36 -0600

    items crontoller created

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::ItemsController < ApplicationController
+  respond_to :json, :xml
+
+  def index
+    respond_with Items.all
+  end
+
+  def show
+    respond_with Items.find_by(id: params[:id])
+  end
 end
