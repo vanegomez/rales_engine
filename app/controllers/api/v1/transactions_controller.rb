868af1043c36871f44763f685ca9c8845c76f5f8
vanegomez <vane.gomez@me.com>
2015-07-28 09:59:04 -0600

    Transaction crontoller created

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::TransactionsController < ApplicationController
+  respond_to :json, :xml
+
+  def index
+    respond_with Transaction.all
+  end
+
+  def show
+    respond_with Transaction.find_by(id: params[:id])
+  end
 end
